aa0992946c6033856e1721a7324a9e992797cfe1
mayank1-0 <mayank.mehmi@gmail.com>
2025-04-03 21:10:17 +0530

    commit-cleaning

--- scripts/shopping-cart.js
+++ scripts/shopping-cart.js
@@ -1,32 +1,34 @@
+//--------------------------------Below is a syntax of Immediately Invoked Function Expression (IIFE)------------------------------------
 var shoppingCart = (function () {
 
-    cart = [];
+    cart = [];                                  // Created an array named cart
 
-    function Item(name, price, count) {
+    function Item(name, price, count) {        // created a function named Item with 3 arguments name, price and count
         this.name = name;
         this.price = price;
         this.count = count;
     }
 
-    // Save cart
+    // Save cart function to store/set data in localStorage
     function saveCart() {
         localStorage.setItem('shoppingCart', JSON.stringify(cart));
     }
 
-    // Load cart
+    // Load cart function to load/retrieve data from localStorage
     function loadCart() {
         cart = JSON.parse(localStorage.getItem('shoppingCart'));
     }
-    if (localStorage.getItem("shoppingCart") != null) {
+
+    if (localStorage.getItem("shoppingCart") != null) {     // if localStorage is not empty we will run the loadCart function which loads the localStorage data into cart array
         loadCart();
     }
 
 
-    var obj = {};
-
+    var obj = {};       // We created a obj named object
+                        // This line is defining a method called addItemToCart on the obj object. In JavaScript, functions can be properties of objects. So, here we are assigning a function to the property addItemToCart of the object obj.
     // Add to cart
-    obj.addItemToCart = function (name, price, count) {
-        for (var item in cart) {
+    obj.addItemToCart = function (name, price, count) {            
+        for (var item in cart) {                              // This is a for in loop where item is index stored in string format and cart is the array containing objects.
             if (cart[item].name === name) {
                 cart[item].count++;
                 saveCart();
@@ -46,7 +48,7 @@ var shoppingCart = (function () {
             }
         }
     };
-    // Remove item from cart
+    // Remove item from cart (Not used anywhere)
     obj.removeItemFromCart = function (name) {
         for (var item in cart) {
             if (cart[item].name === name) {
@@ -92,17 +94,17 @@ var shoppingCart = (function () {
         for (var item in cart) {
             totalCart += cart[item].price * cart[item].count;
         }
-        return Number(totalCart.toFixed(2));
+        return Number(totalCart.toFixed(2));            // Returns number if in decimal number upto two decimal places
     }
 
     // List cart
     obj.listCart = function () {
         var cartCopy = [];
-        for (i in cart) {
-            item = cart[i];
+        for (i in cart) {   // iterating through cart array of objects
+            item = cart[i]; // item is object containing name, count and price key-value pairs
             itemCopy = {};
-            for (p in item) {
-                itemCopy[p] = item[p];
+            for (p in item) {  // p is the key for e.x. name, price, quantity
+                itemCopy[p] = item[p];  // item[p] is the value in key:value pair
             }
             itemCopy.total = Number(item.price * item.count).toFixed(0);
             cartCopy.push(itemCopy)
@@ -112,15 +114,14 @@ var shoppingCart = (function () {
     return obj;
 })();
 
-//--------------------------------
+//--------------------------------Above is a syntax of Immediately Invoked Function Expression (IIFE)------------------------------------
 
 // Add item
 $('.default-btn').click(function (event) {
-    // alert('working');
     event.preventDefault();
     var name = $(this).data('name');
     var price = Number($(this).data('price'));
-    shoppingCart.addItemToCart(name, price, 1);
+    shoppingCart.addItemToCart(name, price, 1);     // Here shoppingCart is the name of the IIFE/object and addItemToCart is the name of the function/key associated with shoppingCart object
     displayCart();
 });
 
@@ -149,6 +150,8 @@ function displayCart() {
     $('.total-cart').html(shoppingCart.totalCart());
     $('.total-count').html(shoppingCart.totalCount());
 }
+
+// Above displayCart function fills the modal and also fill at the top-right, total-count of items
 
 // Delete item button
 $('.show-cart').on("click", ".delete-item", function (event) {
